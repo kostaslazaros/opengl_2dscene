e05a7ef2f9290ad2f7294556587b13c3f6c844a6
konstantinos lazaros <konlazaros@gmail.com>
2021-05-05 01:58:40 +0300

    refactoring

--- src/conf_build_run.sh
+++ src/conf_build_run.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
+rm -rf out/build
+cmake -DGLFW_BUILD_DOCS=OFF -S . -B out/build
 cmake -DGLFW_BUILD_DOCS=OFF -S . -B out/build
 cd out/build ; make ; ./opengl_2dscene
--- src/configure.sh
+++ src/configure.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-
+rm -rf out/build
+cmake -DGLFW_BUILD_DOCS=OFF -S . -B out/build
 cmake -DGLFW_BUILD_DOCS=OFF -S . -B out/build
